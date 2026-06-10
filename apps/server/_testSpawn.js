088ae6bd3cec589fbccf83c5dd02d224bf2ad39f
Admin <admin@multi-store.local>
2026-06-11 01:36:49 +0800

    chore: 清理临时测试文件

--- apps/server/_testSpawn.js
+++
@@ -1,6 +0,0 @@
-﻿const { spawn } = require('child_process');
-const child = spawn('node', ['--version'], { stdio: 'inherit' });
-child.on('exit', (code) => {
-  console.log('node --version exited with code:', code);
-  process.exit(0);
-});
