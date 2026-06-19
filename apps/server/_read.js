cfa22aa52f09f058b33216ff81bce6124ef6a167
九月 <admin@multi-store.local>
2026-06-19 17:59:53 +0800

    v1.1.62: Cleanup temp files

--- apps/server/_read.js
+++
@@ -1,5 +0,0 @@
-﻿const fs = require("fs");
-const p = require("path");
-const docPath = p.join(__dirname, "..", "..", "UPGRADE.md");
-const content = fs.readFileSync(docPath, "utf8");
-console.log(content.substring(0, 2000));
