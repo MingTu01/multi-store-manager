cd28ec8aa378ed2c97627f64934afdba7b374ba6
九月 <admin@multi-store.local>
2026-06-19 16:43:03 +0800

    v1.1.56: Cleanup temp files

--- apps/server/_g.js
+++
@@ -1,1 +0,0 @@
-﻿const fs=require("fs");const s=fs.readFileSync("_b.js","utf8");console.log("File length:",s.length);console.log("First 50:",s.substring(0,50));
--- apps/server/_w.js
+++
@@ -1,1 +0,0 @@
-﻿const fs = require("fs"); const code = fs.readFileSync("_b.js", "utf8"); console.log("First 100:", code.substring(0,100));
